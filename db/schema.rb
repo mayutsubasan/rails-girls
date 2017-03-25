66e9f664c39574c2dc477b7ccbb9807c4c8e46be
mayutsubasan <mayutsuba0874@gmail.com>
2017-03-25 15:38:53 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325024625) do
+ActiveRecord::Schema.define(version: 20170325061911) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
